3c0b489bfbf7c61dbf837339c28c729638714619
Tristen247 <139776061+Tristen247@users.noreply.github.com>
2023-11-20 22:10:57 -0500

    W6

---
+++ W6-VS_Code_Project/script.js
@@ -0,0 +1,26 @@
+var tray = document.querySelector('#tray');
+
+tray.addEventListener('click', openClose)
+
+function openClose(e)
+{
+    var nav = document.querySelector('nav');
+    nav.classList.toggle('hidden');
+}
+
+var tabs = document.querySelectorAll('#tabs a');
+console.log(tabs);
+
+for(let i=0; i < tabs.length; i++)
+{
+    tabs[i].addEventListener('click', function(e){
+        for(let i=0; i < tabs.length; i++)
+        {
+            tabs[i].getElementsByClassName.backgroundcolor = 'orange';
+        }
+        e.target.style.backgroundcolor = `rgb(101, 105, 97)`;
+        document.querySelectorAll('#breadcrumbs a')[3].innerHTML = e.target.innerText
+    });
+}
+
+
